38e0edeff2467ea567de777117be06ef9d646b45
ghgsrt <ajbosco00@gmail.com>
2025-03-03 17:51:03 -0500

    add channels to empty variant

--- guix/bos/home/empty.scm
+++ guix/bos/home/empty.scm
@@ -1,8 +1,22 @@
 (define-module (bos home empty)
+	#:use-module (bos channels)
 	#:use-module (gnu home)
 	#:use-module (gnu home services)
-	#:export (empty-home))
+	#:export (empty-home
+		  empty-home-free
+		  empty-home-non-free))
 
-(define empty-home (home-environment))
+(define* (empty-home #:key (non-free? #f)) 
+  (home-environment
+    (services (modify-services %base-services
+	        (guix-service-type config => (guix-configuration
+	 				       (inherit config)
+					       (channels (if non-free? 
+							   %non-free-channels
+							   %free-channels))))))))
 
-empty-home
+(define empty-home-free (extend-home (empty-home)))
+(define empty-home-non-free (extend-home (empty-home #:non-free? #t)))
+
+empty-home-free
+
