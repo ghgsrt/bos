880dc7f0d27e08da01f9693b3bbadf292cef590f
Alex Bosco <ajbosco00@gmail.com>
2025-03-07 08:44:26 -0500

    Pass through user choice

--- scripts/image.sh
+++ scripts/image.sh
@@ -72,7 +72,9 @@ i_guix() {
         echo "image: using system '$SYSTEM' as image type '$IMAGE_TYPE'"
     fi
 
-      DOTFILES_DIR="$BOS_DOTFILES_DIR" FREE=$using_free SYSTEM_DIR="$BOS_SYSTEM_DIR" TARGET="$SYSTEM" "$1" "$2"\
+	USER="${inputs[6]:-root}"
+
+      USER=$USER DOTFILES_DIR="$BOS_DOTFILES_DIR" FREE=$using_free SYSTEM_DIR="$BOS_SYSTEM_DIR" TARGET="$SYSTEM" "$1" "$2"\
         -L "$BOS_DIR/guix"\
         -L "$BOS_CONFIG_DIR"\
         $3\
